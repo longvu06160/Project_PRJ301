22ff2bdb3f5a99d88c4899a893487d4341b971a6
longvu06160 <92150985+longvu06160@users.noreply.github.com>
2023-10-22 15:21:26 +0700

    Publish to Git

---
+++ Project_PRJ301/src/java/Model/Role.java
@@ -0,0 +1,14 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Model;
+
+/**
+ *
+ * @author vuluo
+ */
+public class Role {
+    int roleId;
+    String roleName;
+}
